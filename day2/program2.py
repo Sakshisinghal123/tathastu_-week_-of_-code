59e27e88a84b58a23600a772b959c778763083ad
Sakshisinghal123 <66126791+Sakshisinghal123@users.noreply.github.com>
2020-05-31 14:36:56 +0530

    Create program2.py

---
+++ day2/program2.py
@@ -0,0 +1,10 @@
+N = int(input("enter the value of N:")
+a = 0
+b = 1
+print("the fibonacci series upto", "th number is following:")
+for i in range(N):
+   print(a, end = " ")
+   c = a + b
+   a = b
+   b = c
+   
